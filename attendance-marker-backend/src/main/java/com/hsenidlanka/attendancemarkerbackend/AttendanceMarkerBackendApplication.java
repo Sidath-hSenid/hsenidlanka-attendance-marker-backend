61f769d26ddfeb3acbd13aceb69b70e556761c2c
sidathrathnayake <sidath.r@hsenidlanka.com>
2023-05-10 22:37:40 +0530

    Created Spring Boot Project

---
+++ attendance-marker-backend/src/main/java/com/hsenidlanka/attendancemarkerbackend/AttendanceMarkerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hsenidlanka.attendancemarkerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceMarkerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceMarkerBackendApplication.class, args);
+	}
+
+}
---
+++ attendance-marker-backend/src/test/java/com/hsenidlanka/attendancemarkerbackend/AttendanceMarkerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsenidlanka.attendancemarkerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceMarkerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
